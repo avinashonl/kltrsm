b8a4817faad6a83f365214cb31d74e1eb358275a
Avinash P <aviputhukulangara@gmail.com>
2024-01-30 10:45:09 +0530

    signup done

--- signup.js
+++ signup.js
@@ -7,7 +7,7 @@ function myFunction() {
       x.type = "password";
     }
 }
-var password = document.getElementById('cpassword');
+var password = document.getElementById('cpass');
       var toggler = document.getElementById('toggler');
 
       showHidePassword = () => {
@@ -25,11 +25,12 @@ var password = document.getElementById('cpassword');
 let name = document.getElementById("name");
 let email = document.getElementById("email");
 let passw = document.getElementById("pass");
+let passc = document.getElementById("cpass");
 let btn1 = document.getElementById("btn1");
 
 const isName = /^[a-zA-Z\s]+$/
 const isEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
-const isPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+const isPass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
 
 btn1.addEventListener('click', ()=>{
     if (name.value.trim() == ''){
@@ -37,17 +38,24 @@ btn1.addEventListener('click', ()=>{
     } else if(!isName.test(name.value.trim())){
         alert("Please enter your Name correctly!");
     }
+
     else if (email.value.trim() == ''){
         alert("Please enter your Email!");
     } else if (!isEmail.test(email.value.trim())){
         alert("Email should be in proper format!");
     } 
+
     else if (pass.value.trim() == ''){
-      alert("Please enter a Password");
-    } else if (!isPass.test(password.value.trim())){
-      alert("Password must contain Capital,Small letter,special charactors!");
+      alert("Please enter a Password!");
+    } else if (!isPass.test(pass.value.trim())){
+      alert("Password must contain minimum of 8 characters and also contain atleast one uppercase letter, one lowercase letter, one number and one special character!");
     }
+    
+    else if (cpass.value.trim() != pass.value.trim()){
+      alert("Password mismatch!");
+    }
+
     else {
-        alert("success");
+        alert("Successfully signed up");
     }
 })
